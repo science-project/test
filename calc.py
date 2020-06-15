84b1a8c42bcee78528a9d9d2c94d46d699eadb82
Fujita Naoki <fna42149@icloud.com>
2020-06-15 14:16:56 +0900

    first commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+def sum(a, b):
+    return a + b
+
+val_1 = input('input first number: ')
+val_2 = input('input second number: ')
+
+value_sum = sum(int(val_1), int(val_2))
+print("")
+
+
+print(value_sum)
